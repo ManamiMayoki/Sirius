1c233f20810801bd2f3f8abb74173a5080af73c7
ManamiMayoki <manamimarufa@gmail.com>
2026-08-25 15:37:02 +0600

    Dog.jsx: import react,three/fiber,three/drei

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,9 +1,3 @@
-import React from 'react'
-import { useGLTF } from '@react-three/drei'
-
-export default function Dog() {
-  const { scene } = useGLTF('/models/dog.glb')
-  return <primitive object={scene} scale={1} />
-}
-
-useGLTF.preload('/models/dog.glb')
+import react from 'react'
+import {Canvas,useThree} from '@react-three/fiber'
+import {OrbitControls, useGLTF} from '@react-three/drei'
