c4901bb8b94598c42f780774af68ce4fdf1012cf
ManamiMayoki <manamimarufa@gmail.com>
2026-08-25 16:08:43 +0600

    Dog.jsx: primitive object rotation Math.PI via y-axis

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -5,9 +5,14 @@ import {OrbitControls, useGLTF} from '@react-three/drei'
 const Dog=()=>{
   const model=useGLTF('/models/dog.drc.glb')
 
+  useThree(({camera,scene,gl})=>{
+    console.log(camera.position)
+    camera.position.z=0.7
+  })
+
   return(
     <>
-        <primitive object={model.scene} position={[0,0,0]}/>
+        <primitive object={model.scene} position={[0,-0.5,0]} rotation={[0,Math.PI,0]}/>
         <directionalLight position={[0,5,5]} color="0xffffff" intensity={10}/>
         <OrbitControls/>
     </>
