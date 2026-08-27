f4b47cef6d0cefcd69624724dd7f136ce184b46c
ManamiMayoki <manamimarufa@gmail.com>
2026-08-27 19:13:38 +0600

    child name

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,20 +1,29 @@
 import react from 'react'
 import {Canvas,useThree} from '@react-three/fiber'
-import {OrbitControls, useGLTF} from '@react-three/drei'
+import * as THREE from 'three'
+import {OrbitControls, useGLTF,useTexture} from '@react-three/drei'
 
 const Dog=()=>{
   const model=useGLTF('/models/dog.drc.glb')
 
   useThree(({camera,scene,gl})=>{
-    console.log(camera.position)
+    // console.log(camera.position)
     camera.position.z=0.55
+  })
+
+  const textures=useTexture({
+    normalMap:"/models/dog_normals.jpg"
+  })
+
+  model.scene.traverse((child)=>{
+    console.log(child.name)
   })
 
   return(
     <>
         <primitive object={model.scene} position={[0.25,-0.55,0]} rotation={[0.25,Math.PI/3.9,0]}/>
         <directionalLight position={[0,5,5]} color="0xffffff" intensity={10}/>
-        <OrbitControls/>
+        {/* <OrbitControls/> */}
     </>
   )
 }
