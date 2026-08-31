0b10b50be1ba96369fa0e6e4a5a384840d57892d
ManamiMayoki <manamimarufa@gmail.com>
2026-08-31 21:29:11 +0600

    Dog.jsx: branchMaterial function

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,10 +1,10 @@
-import React,{useEffect} from 'react'
-import {Canvas,useThree} from '@react-three/fiber'
+import React, { useEffect } from 'react'
+import { Canvas, useThree } from '@react-three/fiber'
 import * as THREE from 'three'
-import {OrbitControls, useGLTF,useTexture,useAnimations} from '@react-three/drei'
+import { OrbitControls, useGLTF, useTexture, useAnimations } from '@react-three/drei'
 
-const Dog=()=>{
-  const model=useGLTF('/models/dog.drc.glb')
+const Dog = () => {
+  const model = useGLTF('/models/dog.drc.glb')
 
 
   // const {camera,scene,gl}=useThree();
@@ -12,17 +12,17 @@ const Dog=()=>{
   //   camera.positionz=0.55
   // })
 
-  useThree(({camera,scene,gl})=>{
+  useThree(({ camera, scene, gl }) => {
     // console.log(camera.position)
-    camera.position.z=0.7
+    camera.position.z = 0.7
     // gl.toneMapping=THREE.ReinhardToneMapping
     // gl.outputColorSpace=THREE.SRGBColorSpace
   })
 
-  const {actions}=useAnimations(model.animations,model.scene)
-  useEffect(()=>{
+  const { actions } = useAnimations(model.animations, model.scene)
+  useEffect(() => {
     actions["Take 001"]?.play()
-  },[actions])
+  }, [actions])
 
   // const textures=useTexture({
   //   normalMap:"/models/dog_normals.jpg",
@@ -30,49 +30,54 @@ const Dog=()=>{
   // })
 
 
-// textures.normalMap.flipY=false
+  // textures.normalMap.flipY=false
   // textures.sampleMatCap.colorSpace=THREE.SRGBColorSpace
 
 
   //optimized code
-  const[
+  const [
     normalMap,
     sampleMatCap,
     branchMap,
     branchNormalMap
-  ]=(useTexture(["/models/dog_normals.jpg","matcap/mat-2.png","/models/branches_diffuse.dds","/models/branches_normals.dds"]))
-  .map(texture=>{
-    texture.flipY=false
-    texture.colorSpace=THREE.SRGBColorSpace
-    return texture
-  })
+  ] = (useTexture(["/models/dog_normals.jpg", "matcap/mat-2.png", "/models/branches_diffuse.dds", "/models/branches_normals.dds"]))
+    .map(texture => {
+      texture.flipY = false
+      texture.colorSpace = THREE.SRGBColorSpace
+      return texture
+    })
 
-  
 
-  const dogMaterial=new THREE.MeshMatcapMaterial({
+
+  const dogMaterial = new THREE.MeshMatcapMaterial({
     normalMap: normalMap,
     matcap: sampleMatCap
   })
 
-  model.scene.traverse((child)=>{
+  const branchMaterial=new THREE.MeshMatcapMaterial({
+    normalMap: branchNormalMap,
+    map: branchMap
+  })
+
+  model.scene.traverse((child) => {
     // console.log(child.name)
-    if(child.name.includes("DOG")){
+    if (child.name.includes("DOG")) {
       // console.log(child.name)
       // child.material=new THREE.MeshMatcapMaterial({
       //   // normalMap: normalMap,
       //   // matcap: sampleMatCap
       //   // color:"#800000"
       // })
-      child.material=dogMaterial
+      child.material = dogMaterial
     }
   })
 
 
-  return(
+  return (
     <>
-        <primitive object={model.scene} position={[0.25,-0.55,0]} rotation={[0.25,Math.PI/3.9,0]}/>
-        <directionalLight position={[0,5,5]} color="#ffffff" intensity={10}/>
-        {/* <OrbitControls/> */}
+      <primitive object={model.scene} position={[0.25, -0.55, 0]} rotation={[0.25, Math.PI / 3.9, 0]} />
+      <directionalLight position={[0, 5, 5]} color="#ffffff" intensity={10} />
+      {/* <OrbitControls/> */}
     </>
   )
 }
